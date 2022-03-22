c1ad6eea1d9aa8846fd288d83780d6d27c2a2132
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-22 11:23:06 -0700

    created fetch-utils.js file where client API key and async function getPizzas() is stored. In index.html added supabase CDN on line 12.

---
+++ fetch-utils.js
@@ -0,0 +1,12 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tZ21ka3dhdGNtcXJleHZ3aHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5NzA2MTcsImV4cCI6MTk2MzU0NjYxN30.3xHneXPhk8TMzUGRgqhQh9EMm_NmROG5YLi0ZnrTNUY';
+const SUPABASE_URL = 'https://nmgmdkwatcmqrexvwhql.supabase.co';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getPizzas() {
+    const response = await client
+        .from('pizzas')
+        .select('*');
+
+    return response.body;
+}
