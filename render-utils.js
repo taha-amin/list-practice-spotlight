104eb9739c1f05b11b1506e3ffb073e24fa92ea3
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-22 11:36:14 -0700

    created render-utils.js file that contains renderPizza function and creates all needed DOM elements.

---
+++ render-utils.js
@@ -0,0 +1,21 @@
+export function renderPizza(pizza) {
+    const pizzaEl = document.createElement('div');
+    const priceEl = document.createElement('p');
+    const toppingsEl = document.createElement('p');
+    const sizeEl = document.createElement('p');
+    const crustEl = document.createElement('p');
+
+    pizzaEl.classList.add('pizza');
+
+    priceEl.textContent = pizza.price;
+    toppingsEl.textContent = pizza.toppings;
+    sizeEl.textContent = pizza.size;
+    crustEl.textContent = pizza.crust;
+
+    pizzaEl.append(
+        priceEl,
+        toppingsEl,
+        sizeEl,
+        crustEl
+    );
+}
